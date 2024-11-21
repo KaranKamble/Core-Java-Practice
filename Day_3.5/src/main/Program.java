3b4cc74e2bbd4881a8aab2420ed668aac5e3b272
Karan Kamble <karanbk9375@gmail.com>
2024-11-21 12:11:08 +0530

    commit

---
+++ Day_3.5/src/main/Program.java
@@ -0,0 +1,18 @@
+package main;
+
+public class Program {
+	public static void main1(String[] args) {
+		for( int i = 1; i <=10; i++ )
+			System.out.print(i+" ");
+	}
+	
+	public static void main(String[] args) {
+		int count = 1;
+		while( count < 11 ) {
+			System.out.print(count+" ");
+			count++;
+		}
+			
+	}
+
+}
