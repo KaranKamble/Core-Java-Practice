7a1525f4d608df2067892256740626652b8d5334
Karan Kamble <karanbk9375@gmail.com>
2024-12-07 20:01:35 +0530

    enum, static import

---
+++ Day_5.1/src/main/Program.java
@@ -0,0 +1,23 @@
+package main;
+
+import java.util.Scanner;
+import static java.lang.Math.*;
+import static java.lang.System.*;
+
+public class Program {
+
+	public static void main1(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Lets Calculate Area od Circle ");
+		System.out.println("Enter radius  :  ");
+		int radius = sc.nextInt();
+		double area = PI*pow(radius, 2);
+		System.out.println("Area : "+area);
+	}
+	
+	public static void main(String[] args) {
+		out.println("Enter Name : ");
+	}
+
+}
