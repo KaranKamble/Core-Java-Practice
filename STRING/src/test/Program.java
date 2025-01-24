f900a4482e325f139efc1a6f743db8081fee0e02
Karan Kamble <karanbk9375@gmail.com>
2025-01-24 16:11:40 +0530

    Reverse the string

---
+++ STRING/src/test/Program.java
@@ -0,0 +1,16 @@
+package test;
+
+public class Program {
+	public static void main(String[] args) {
+		String str = "Hello World!";
+		System.out.println(str);
+		
+		String reverse = "";
+		
+		for( int i = str.length() - 1; i >= 0; i -- ) 
+			reverse = reverse + str.charAt(i);
+		
+		System.out.println(reverse);
+	}
+
+}
