0c5947c1a00ed2af9489c0cf059a01f7897f2364
Karan Kamble <karanbk9375@gmail.com>
2025-01-24 15:51:42 +0530

    Hello world without ;

--- Day_20.3/src/test/Program.java
+++ Day_20.3/src/test/Program.java
@@ -2,16 +2,21 @@ package test;
 
 public class Program {
 	private static int num;
-	
+
 	public static void print() {
 		num++;
 		System.out.println(Program.num);
 	}
 
-	public static void main(String[] args) {
+	public static void main1(String[] args) {
 		Program.print();
 		Program.print();
 		Program.print();
 	}
 
+	public static void main(String[] args) {
+
+		if (System.out.printf("Hello World") != null) {}
+		
+	}
 }
