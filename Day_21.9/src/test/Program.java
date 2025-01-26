583dfeefdb0001903d1ade30f6b3679959b4d6b6
Karan Kamble <karanbk9375@gmail.com>
2025-01-26 23:59:13 +0530

    program to print all duplicate character and their count from the given String

--- Day_21.9/src/test/Program.java
+++ Day_21.9/src/test/Program.java
@@ -3,6 +3,31 @@ package test;
 public class Program {
 
 	public static void main(String[] args) {
+		// write a program to print all duplicate character and their count from the
+		// given String
+
+		String str = "Programming";
+		String check = "";
+
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+
+			if (check.indexOf(ch) == -1) {
+				int count = 1;
+				for (int j = 0; j < str.length(); j++) {
+					if (i != j && ch == str.charAt(j)) {
+						check = check+ch;
+						count++;
+					}
+				}
+				if( count > 1 )
+				System.out.println(ch + " " + count);
+			}
+		}
+
+	}
+
+	public static void main1(String[] args) {
 		// Program to print duplicate element from given string
 
 		String str = "Programming";
@@ -13,7 +38,7 @@ public class Program {
 			if (duplicate.indexOf(ch) == -1) {
 				for (int j = 0; j < str.length(); j++) {
 					if (i != j && ch == str.charAt(j)) {
-						duplicate = duplicate + ch+" ";
+						duplicate = duplicate + ch + " ";
 						break;
 					}
 				}
